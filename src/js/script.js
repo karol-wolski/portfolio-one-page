06144e6e78b623d3137eba9ec5b75e3bb1277541
Karol Wolski <karolwolski04@gmail.com>
2021-08-30 23:53:24 +0200

    feat: Section About
    Add scroll to section function
    Add section about

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 import '../scss/style.scss'
 import mobileMenu from './mobile-menu'
+import scrollToSection from './scrollToSection'
 
 const init = () => {
   const navigationButton = document.querySelector('#button-menu')
   navigationButton.addEventListener('click', mobileMenu)
+  scrollToSection()
 }
 
 init()
